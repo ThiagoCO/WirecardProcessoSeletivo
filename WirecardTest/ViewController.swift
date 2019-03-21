e2ba0c7670dbfe3179ee4351a4a7b65d7ac6164c
Thiago Cavalcante <thiago.cavalcante15@hotmail.com>
2019-03-20 21:11:59 -0300

    Initial Commit

---
+++ WirecardTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WirecardTest
+//
+//  Created by Thiago Cavalcante de Oliveira on 20/03/19.
+//  Copyright © 2019 Thiago Cavalcante de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
